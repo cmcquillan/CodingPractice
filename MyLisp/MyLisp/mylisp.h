c320f900da3dda1121accc9db92b063af7e056d6
Casey McQuillan <casey.mcquillan@gmail.com>
2014-04-12 17:33:23 -0400

    Added support for S-Expressions

--- MyLisp/MyLisp/mylisp.h
+++ MyLisp/MyLisp/mylisp.h
@@ -1,19 +1,33 @@
-typedef struct {
+typedef struct lval {
     int type;
+
     long num;
-    int err;
+
+
+    char* err;
+    char* sym;
+
+    int count;
+
+    struct lval** cell;
+
 } lval;
 
-lval eval(mpc_ast_t* );
-lval eval_op(lval, char*, lval);
+void lval_del(lval*);
+lval* builtin_op(lval*, char*);
+lval* lval_take(lval*, int);
+lval* lval_pop(lval*, int);
+lval* lval_eval(lval*);
+lval* lval_eval_sexpr(lval*);
+void lval_expr_print(lval*, char, char);
 int count_ast_leaves(mpc_ast_t*);
 int count_ast_branches(mpc_ast_t*);
 long l_pow(long, long);
-lval lval_num(long x);
-lval lval_err(int x);
-void lval_print(lval v);
-void lval_println(lval v);
+lval* lval_num(long);
+lval* lval_err(char*);
+void lval_print(lval*);
+void lval_println(lval*);
 
-enum { LVAL_NUM, LVAL_ERR };
+enum lval_type { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR };
 
-enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+enum lerr_type { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
