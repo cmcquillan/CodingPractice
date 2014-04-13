3f84d78bf25381879d31bedd5f73c11cd45357a7
Casey McQuillan <casey.mcquillan@gmail.com>
2014-04-13 13:47:06 -0400

    Created the symbol table and environment structure.

--- MyLisp/MyLisp/mylisp.h
+++ MyLisp/MyLisp/mylisp.h
@@ -1,45 +1,72 @@
-typedef struct lval {
-    int type;
+struct lval;
+struct lenv;
+typedef struct lval lval;
+typedef struct lenv lenv;
+typedef enum lval_type lval_type;
+typedef enum lerr_type lerr_type;
+
+enum lval_type { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR, LVAL_FUN };
+enum lerr_type { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+typedef lval*(*lbuiltin)(lenv*, lval*);
+
+struct lval {
+    lval_type type;
 
     long num;
-
-
     char* err;
     char* sym;
+    lbuiltin fun;
 
     int count;
+    lval** cell;
 
-    struct lval** cell;
+};
 
-} lval;
+struct lenv {
+	int count;
+	char** syms;
+	lval** vals;
+};
 
 void lval_del(lval*);
-lval* builtin_op(lval*, char*);
+lval* builtin_op(lenv*, lval*, char*);
 lval* lval_take(lval*, int);
 lval* lval_pop(lval*, int);
-lval* lval_eval(lval*);
-lval* lval_eval_sexpr(lval*);
+lval* lval_copy(lval*);
+lval* lval_eval(lenv*, lval*);
+lval* lval_eval_sexpr(lenv*, lval*);
 void lval_expr_print(lval*, char, char);
 int count_ast_leaves(mpc_ast_t*);
 int count_ast_branches(mpc_ast_t*);
 long l_pow(long, long);
 lval* lval_num(long);
+lval* lval_sym(char*);
+lval* lval_fun(lbuiltin);
 lval* lval_err(char*);
 void lval_print(lval*);
 void lval_println(lval*);
-lval* builtin(lval*, char*);
-lval* builtin_head(lval*);
-lval* builtin_list(lval*);
-lval* builtin_eval(lval*);
-lval* builtin_join(lval*);
+
+//lval* builtin(lval*, char*);
+lval* builtin_head(lenv*, lval*);
+lval* builtin_list(lenv*, lval*);
+lval* builtin_eval(lenv*, lval*);
+lval* builtin_join(lenv*, lval*);
+lval* builtin_add(lenv*, lval*);
+lval* builtin_tail(lenv*, lval*);
+lval* builtin_sub(lenv*, lval*);
+lval* builtin_div(lenv*, lval*);
+lval* builtin_mul(lenv*, lval*);
+lval* builtin_mod(lenv*, lval*);
 lval* lval_join(lval*, lval*);
-lval* builtin_tail(lval*);
 lval* lval_add(lval*, lval*);
-lval* builtin_len(lval*);
+lval* builtin_len(lenv*, lval*);
 
-enum lval_type { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR };
-
-enum lerr_type { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
-
+void lenv_add_builtin(lenv*, char*, lbuiltin);
+void lenv_add_builtins(lenv*);
+void lenv_put(lenv*, lval*, lval*);
+lval* lenv_get(lenv*, lval*);
+void lenv_del(lenv*);
+lenv* lenv_new(void);
 
 #define LASSERT(args, cond, err) if(!(cond)) { lval_del(args); return lval_err(err); }
